5827be24355fc89f61161b5d555bc29f0b3899a4
MazharSolkar <mazharsolkar65@gmail.com>
2024-04-18 10:50:33 +0530

    Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { CiMenuBurger } from 'react-icons/ci';
+import { IoClose } from 'react-icons/io5';
+import { MdShoppingCart } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const Navbar = () => {
+	const [isOpen, setisOpen] = useState(false);
+	const totalItem = useSelector((state) => state.cartSlice.cartItems.length);
+
+	const links = [
+		{ name: 'Home', path: '/' },
+		{ name: 'About', path: '/about' },
+		{ name: 'Cart', path: '/cart' },
+	];
+	return (
+		<div className='bg-white fixed w-full top-0 left-[50%] translate-x-[-50%]  z-10 shadow-lg text-gray-800'>
+			<nav className='md:flex justify-between items-center max-w-screen-xl mx-auto'>
+				<div className='header flex justify-between items-center py-2'>
+					<h1 className='text-2xl font-bold flex justify-center items-center gap-2 relative'>
+						Urban Market
+						<Link to='/cart'>
+							<MdShoppingCart className='text-4xl text-orange-500 cursor-pointer' />
+						</Link>
+						<p className=' absolute top-[-8px] right-[-17px] bg-black text-lg text-white rounded-full px-[2px] h-6 w-6 flex justify-center items-center'>
+							{totalItem}
+						</p>
+					</h1>
+
+					<div
+						className='burger text-2xl md:hidden'
+						onClick={() => setisOpen(!isOpen)}>
+						{isOpen ? <IoClose /> : <CiMenuBurger />}
+					</div>
+				</div>
+				<ul
+					className={`w-full md:w-auto md:flex text-xl font-semibold absolute z-20 md:static ${
+						isOpen ? 'top-[40px]' : 'top-[-220px]'
+					} transition-all ease-in delay-300 `}>
+					{links.map((link) => (
+						<NavLink
+							className={({ isActive }) => (isActive ? 'underline' : '')}
+							to={link.path}
+							key={link.name}>
+							<p className='my-2 py-1 md:mx-2 md:px-2 md:py-1 hover:underline'>
+								{link.name}
+							</p>
+						</NavLink>
+					))}
+				</ul>
+			</nav>
+		</div>
+	);
+};
+
+export default Navbar;
